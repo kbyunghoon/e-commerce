e166d9246f29c0bd74dd83ce8ba3d03b4844bc46
kbyunghoon <dev.kbhoon@gmail.com>
2025-08-06 18:07:04 +0900

    feat: 선착순 쿠폰 발급 관련 비관적락 동시성 제어 기능 구현

--- src/main/kotlin/kr/hhplus/be/infrastructure/persistence/repository/CouponJpaRepository.kt
+++ src/main/kotlin/kr/hhplus/be/infrastructure/persistence/repository/CouponJpaRepository.kt
@@ -1,9 +1,16 @@
 package kr.hhplus.be.infrastructure.persistence.repository
 
+import jakarta.persistence.LockModeType
 import kr.hhplus.be.infrastructure.entity.CouponEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CouponJpaRepository : JpaRepository<CouponEntity, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM CouponEntity c WHERE c.id = :id")
+    fun findByIdWithPessimisticLock(@Param("id") productId: Long): CouponEntity?
 }
